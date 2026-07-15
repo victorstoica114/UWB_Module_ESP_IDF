94978310ac9a0b1aa9f9f46897c21eb355ab30d3
Victor Stoica <victorstoica114@gmail.com>
2026-07-15 12:43:04 +0300

    Move diagnostic buffers to PSRAM

--- components/resource_monitor_service/resource_monitor_service.c
+++ components/resource_monitor_service/resource_monitor_service.c
@@ -437,16 +437,17 @@ esp_err_t resource_monitor_service_start(void)
     (void)init_temperature_sensor();
     (void)init_task_sampling_buffers();
 
-    const BaseType_t created = xTaskCreatePinnedToCore(
+    const BaseType_t created = xTaskCreatePinnedToCoreWithCaps(
         resource_monitor_task, "resource_mon",
         RESOURCE_MONITOR_TASK_STACK_WORDS, NULL,
-        RESOURCE_MONITOR_TASK_PRIORITY, NULL, RESOURCE_MONITOR_TASK_CORE);
+        RESOURCE_MONITOR_TASK_PRIORITY, NULL, RESOURCE_MONITOR_TASK_CORE,
+        MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
     if (created != pdPASS) {
         return ESP_ERR_NO_MEM;
     }
 
     s_started = true;
-    ESP_LOGI(TAG, "Resource monitor started: interval=%ums core=%d",
+    ESP_LOGI(TAG, "Resource monitor started: interval=%ums core=%d stack=PSRAM",
              (unsigned)RESOURCE_MONITOR_SAMPLE_MS, RESOURCE_MONITOR_TASK_CORE);
     return ESP_OK;
 }
